0ec3dd642a0045e19b5e7279d495245f0c156533
slazarou <sotirakis@gmail.com>
2022-12-10 11:01:34 +0200

    day10

--- src/main/rust/advent2022/day10.rs
+++ src/main/rust/advent2022/day10.rs
@@ -1,14 +1,14 @@
 use std::fs::read_to_string;
 
-const MAX_CYCLES: i32 = 221;
+const MAX_CYCLES: i32 = 240;
 const INTERESTING_CYCLES: [i32; 6] = [20, 60, 100, 140, 180, 220];
+const CRT_LINE_SIZE: i32 = 40;
 
 #[derive(Copy, Clone, Debug)]
 enum Instruction {
     ADD(i32, i32), // amount, cycles to complete
     NOOP(i32) // cycles to complete
 }
-
 
 fn to_instruction(line: &str) -> Instruction {
     match line.split(' ').collect::<Vec<_>>()[..] {
@@ -24,6 +24,36 @@ struct CPU {
     register: i32
 }
 
+struct CRT {
+    lines: Vec<Vec<char>>
+}
+
+impl CRT {
+    pub fn new() -> Self {
+        Self {
+            lines: vec![vec!['.';40];6],
+        }
+    }
+
+    fn draw(&mut self, drawing_position: i32, sprite_mid_point: i32) {
+        let row = drawing_position / CRT_LINE_SIZE;
+        let column = drawing_position % CRT_LINE_SIZE;
+
+        if sprite_mid_point-1<= column && column <= sprite_mid_point+1 {
+            self.lines[row as usize][column as usize] = '#';
+        }
+    }
+
+    fn print(&self) {
+        for row in 0..6 {
+            for column in 0..40 {
+                print!("{}", self.lines[row][column]);
+            }
+            println!();
+        }
+    }
+}
+
 fn main() {
     let contents = read_to_string("inputs/advent2022/day10")
         .expect("Should have been able to read the file");
@@ -34,14 +64,18 @@ fn main() {
         register: 1
     };
 
+    let mut crt = CRT::new();
+
     let mut signal = 0;
     for cycle in 1..MAX_CYCLES {
         start_of_cycle(&mut cpu);
-        during_cycle(&mut cpu, cycle, &mut signal);
+        during_cycle(&mut cpu, cycle, &mut signal, &mut crt);
         end_of_cycle(&mut cpu);
     }
 
     println!("Part 1 {signal}");
+    println!("Part 2:");
+    crt.print();
 }
 
 fn start_of_cycle(cpu: &mut CPU) {
@@ -51,11 +85,13 @@ fn start_of_cycle(cpu: &mut CPU) {
     }
 }
 
-fn during_cycle(cpu: &mut CPU, cycle: i32, signal: &mut i32) {
+fn during_cycle(cpu: &mut CPU, cycle: i32, signal: &mut i32, crt: &mut CRT) {
 
     if INTERESTING_CYCLES.contains(&cycle){
         *signal += cycle * cpu.register;
     }
+
+    crt.draw(cycle-1, cpu.register);
 }
 
 fn end_of_cycle(cpu: &mut CPU) {
